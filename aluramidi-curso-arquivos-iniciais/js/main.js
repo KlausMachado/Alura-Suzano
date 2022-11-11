e4ee96cb3ee040896c879d41aac26d3a63eb9806
Carla Machado <carlamrosa438@gmail.com>
2022-11-11 18:30:34 -0300

    updates midiAlura

--- aluramidi-curso-arquivos-iniciais/js/main.js
+++ aluramidi-curso-arquivos-iniciais/js/main.js
@@ -4,16 +4,31 @@ let count = 0;
 console.log(getButtons);
 
 function playSounds(idElementAudio) {
-  document.querySelector(idElementAudio).play();
+  const element = document.querySelector(idElementAudio);
+
+  if (element && element.localName === `audio`) {
+    element.play();
+  } else {
+    console.log(`Elemento n'ao encontrado`);
+  }
 }
 
 for (let count = 0; count < getButtons.length; count++) {
   const midiKey = getButtons[count];
   const midi = midiKey.classList[1];
-
   const idAudio = `#som_${midi}`;
 
   midiKey.onclick = function () {
     playSounds(idAudio);
+  };
+
+  midiKey.onkeydown = function (event) {
+    if (event.code === `Space` || event.code === `Enter`) {
+      midiKey.classList.add("ativa");
+    }
+  };
+
+  midiKey.onkeyup = function () {
+    midiKey.classList.remove("ativa");
   };
 }
